746d76e84f8010f17035b5ce839bf029e6a434da
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:21:15 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1511C | Yet Another Card Deck/209102413.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    ll size , q;
+    cin >> size >> q;
+    map< ll , ll >freq;
+    for (int i = 1; i <= size; ++i) {
+        ll x ;
+        cin >> x;
+        if( !freq[ x ] )
+            freq[ x ] = i;
+    }
+    while( q-- )
+    {
+        ll x;
+        cin >> x;
+        cout<<freq[ x ]<<" ";
+        if( freq[ x ] == 1 )
+            continue;
+        ll idx = freq[x];
+        for(auto &it : freq)
+        {
+            if( it.second < idx )
+            {
+                it.second++;
+            }
+        }
+        freq[ x ] = 1;
+    }
+    return 0;
+}
