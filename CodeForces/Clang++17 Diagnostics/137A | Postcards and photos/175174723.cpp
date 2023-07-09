e0686a09b8107380b59299d8f009b61a38e7f99d
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:49:32 +0300

    Committing files

---
+++ CodeForces/Clang++17 Diagnostics/137A | Postcards and photos/175174723.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#define MAX 500
+using namespace std;
+ 
+int main()
+{
+    string word="CPCPCPC";
+    cin>>word;
+    int start=0,count=0,end=1;
+    char ch=word[0];
+    while(word.size())
+    {
+        if(ch != word[end])
+        {
+            word.erase(0,end);
+            ch=word[0];
+            end=1;
+            count++;
+        }
+        else if(end == 4)
+        {
+            word.erase(0,end+1);
+            ch=word[0];
+            end=1;
+            count++;
+        }
+        else
+        {
+            end++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
