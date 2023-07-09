390db723a982d04504a6679027efeab911c3b165
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:45:17 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1521A | Nastia and Nearly Good Numbers/188200851.cpp
@@ -0,0 +1,63 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_IO(x)          ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define fraction            cout<< fixed <<showpoint <<setprecision
+#define what_is(x)          cerr << #x << " is " << x << "\n"
+#define PI                  3.141592653589793238462643383279
+#define all(x)              x.begin(),x.end()
+#define Reverse(x)          reverse(all(x))
+#define Sort(x)             sort(all(x))
+#define ln                  endl
+typedef long long           ll;
+ 
+//**prototype**//
+inline void _GOM3A_(void);
+//**prototype**//
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll tt;
+    cin >> tt;
+    while(tt--)
+    {
+        ll a,b;
+        cin >> a >> b;
+        if (b == 1) { 
+            cout<<"no\n";
+            continue;
+        }
+        ll num = 1;
+        ll mul = a * b;
+        while(true)
+        {
+            if ( (a * num % mul != 0) && ( ( a * (b+num) ) % mul != 0) )
+            {
+                cout<<"yes"<<ln;
+                cout<<a * num<<" "<<a * b<<" "<<a * (b+num)<<ln;
+                break;
+            }
+            num++;
+        }
+ 
+ 
+ 
+    }
+    //********** code ***************
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(7);
+    fast_IO(  author: _GOM3A_ );
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+}
