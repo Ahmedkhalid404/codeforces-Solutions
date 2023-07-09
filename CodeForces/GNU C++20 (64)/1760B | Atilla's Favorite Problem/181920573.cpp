3290806350007ab73398a9cba1639a3f22ea808d
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:47:49 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1760B | Atilla's Favorite Problem/181920573.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define yes cout<<"yes"
+#define Yes cout<<"YES"
+#define no cout<<"no"
+#define No cout<<"NO"
+#define ln endl
+#define MAX 500
+#define fraction cout<< fixed <<showpoint <<setprecision
+#define fast_IO() ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+int main() {
+    fraction(9);//how many digits after the decimal point
+    fast_IO();// make cout and cin more fast
+    //code
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll size;
+        cin>>size;
+        string s;
+        cin>>s;
+        sort( s.begin(),s.end() );
+        cout<< ( (s[ s.size()-1 ] - '0') - ('a' - '0')) + 1<<ln;
+    }
+    //code
+    return 0;
+}
+/*
+[#]=============================================================[#]
+             ___    ____              ____          ___ _
+   |_ _|  / ___|__ _ _ __   |  _ \  ___   |_ _| |_
+    | |  | |   / _` | '_ \  | | | |/ _ \   | || __|
+    | |  | |__| (_| | | | | | |_| | (_) |  | || |_
+   |___|  \____\__,_|_| |_| |____/ \___/  |___|\__|
+       it's just about time
+ 
+[#]=============================================================[#]
+*/
