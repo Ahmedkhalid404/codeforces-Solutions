9d5812c49755c2a7ff653cc5b324e01adc454bbc
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:42:43 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/977B | Two-gram/191534429.cpp
@@ -0,0 +1,81 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_IO(x)              ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define fraction                cout<< fixed <<showpoint <<setprecision
+#define what_is(x)              cerr << #x << " is " << x << '\n';
+#define PI                      3.141592653589793238462643383279
+#define all(x)                  x.begin(),x.end()
+#define Reverse(x)              reverse(all(x))
+#define Sort(x)                 sort(all(x))
+#define Yes                     cout<<"YES"
+#define yes                     cout<<"yes"
+#define No                      cout<<"NO"
+#define no                      cout<<"no"
+#define ln                      '\n'
+typedef deque<long long>        dll;
+typedef vector<long long>       vll;
+typedef set<long long>          sll;
+typedef long long               ll;
+ 
+//**prototype**//
+inline void _GOM3A_(void);
+template<typename T, typename ...Opts>
+bool any_of(T val, Opts ...opts);
+template<typename T>
+void doSomeThing(T &num );
+//**prototype**//
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll size;
+    string s;
+    map<string , ll>dp;
+    cin >> size >> s;
+    for (int i = 0; i < size - 1; ++i) {
+        dp[ s.substr(i,2) ]++;
+    }
+    pair<string , ll> mxSt;
+    mxSt.second = 0;
+    for(auto it : dp)
+    {
+        if( mxSt.second < it.second )
+        {
+            mxSt.first = it.first;
+            mxSt.second = it.second;
+        }
+    }
+    cout<<mxSt.first;
+    //********** code ***************
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(6);
+    fast_IO(  author: _GOM3A_ );
+ 
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+}
+template<typename T,typename ...Opts>
+bool any_of(T val, Opts ...opts)
+{
+    return ( ... || (val == opts) );
+}
+template<typename T>
+void doSomeThing(T & num ){
+    cin >> num;
+}
+ 
+ 
+ 
+ 
+ 
