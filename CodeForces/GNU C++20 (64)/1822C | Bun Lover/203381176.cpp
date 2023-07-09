e397a91aadc86397f401a23faba33634f1941ede
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:23:58 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1822C | Bun Lover/203381176.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long tt;
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    cin >> tt;
+    while( tt-- )
+    {
+        long long x;
+        cin>>x;
+        cout<<x*x+(2*x+2)<<'\n';
+    }
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
