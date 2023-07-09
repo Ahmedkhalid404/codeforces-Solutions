e385c9f41f63cac6730ba63b8f687c106ef51365
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:21:30 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1062A | A Prank/208597691.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    ll mx = 0;
+    ll size ; cin >> size;
+    vector< ll >numbers( size );
+    for (int i = 0; i < size; ++i) {
+        cin >> numbers[i];
+    }
+    if( size == 1 )
+        return cout<<0,0;
+    for (int i = 0; i < size; ++i) {
+        ll semiMx = 0;
+        int j = i;
+        for (; j < size; ++j) {
+            if( !j )
+            {
+                if(  numbers[j + 1] -numbers[j] == 1 && numbers.front() == 1)
+                    semiMx++;
+                else
+                    break;
+            }
+            else if ( j == size - 1 )
+            {
+                if( numbers[j] - numbers[j - 1] == 1 && numbers.back() == 1000 )
+                    semiMx++;
+                else
+                    break;
+            }
+            else
+            {
+                if( ( numbers[j + 1] - numbers[j] == 1 ) && ( numbers[j] - numbers[j-1] == 1 ) )
+                    semiMx++;
+                else
+                    break;
+            }
+        }
+        i = j;
+        mx = (ll)max( mx , semiMx );
+    }
+    cout<< ( mx == size ? mx - 1 : mx );
+    return 0;
+}
