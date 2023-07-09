91f159bb77438144e12873713443e3634dea4a1e
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:17:53 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1846E1 | Rudolf and Snowflakes (simple version)/212938139.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    ll tt;
+    cin >> tt;
+    while( tt-- )
+    {
+        ll n;
+        cin >> n;
+        ll k = 2;
+        while( true )
+        {
+            ll res = ( ( (k * k) + k ) + 1) ;
+            ll curr = k * k;
+            while( res < n )
+            {
+                res = res + ( curr * k );
+                curr = curr * k;
+            }
+            //cout<<res<<'\n';
+            if( res == n )
+            {
+                cout<<"YES\n";
+                goto finishTestCase;
+            }
+            res = ( ( (k * k) + k ) + 1) ;
+            if( res > n )
+            {
+                cout<<"NO\n";
+                goto finishTestCase;
+            }
+            k++;
+        }
+        finishTestCase:;
+    }
+    return 0;
+}
