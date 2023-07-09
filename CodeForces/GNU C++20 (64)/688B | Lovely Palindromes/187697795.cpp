7aca7afac28635ad27f123744c0121c8c9b28c71
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:45:33 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/688B | Lovely Palindromes/187697795.cpp
@@ -0,0 +1,46 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define Sort(x) sort(all(x))
+#define Reverse(x) reverse(all(x))
+#define ln endl
+#define MAX 500
+#define fraction cout<< fixed <<showpoint <<setprecision
+#define fast_IO(x) ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << " ";
+//#define ONPC
+using namespace std;
+void ReverseNormal(string s);
+int main() {
+    fraction(7);
+    fast_IO(  author: _GOM3A_ );
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+    string s;
+    cin >> s;
+    ReverseNormal(s);
+}
+ 
+//****************************************    helping function    ****************************************
+bool sortbyseconde(const  pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.second>b.second;
+}
+bool done = false;
+void ReverseNormal(string s)
+{
+    cout << s;
+    if(done)
+        return;
+    Reverse(s);
+    done = ! done;
+    ReverseNormal(s);
+}
