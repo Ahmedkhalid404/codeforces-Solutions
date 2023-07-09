484cadfd554a1da3fb9cf8375cb7ca70cacefde4
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:47:59 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/467A | George and Accommodation/181854814.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define yes cout<<"yes"
+#define Yes cout<<"YES"
+#define no cout<<"no"
+#define No cout<<"NO"
+#define ln endl
+#define MAX 500
+#define fraction cout<< fixed <<showpoint <<setprecision
+#define fast_IO() ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+int main() {
+    fraction(9);//how many digits after the decimal point
+    fast_IO();// make cout and cin more fast
+    //code
+    ll rooms;   cin>>rooms;
+    ll count = 0;
+    for(int i=0;i<rooms;i++)
+    {
+        ll num1,num2;   cin>>num1>>num2;
+        if( num2 - num1 >= 2 )
+            count++;
+    }
+    cout<<count;
+ 
+    //code
+    return 0;
+}
+/*
+        [#]=============================================================[#]
+    
+   |_ _|  / ___|__ _ _ __   |  _ \  ___   |_ _| |_
+    | |  | |   / _` | '_ \  | | | |/ _ \   | || __|
+    | |  | |__| (_| | | | | | |_| | (_) |  | || |_
+   |___|  \____\__,_|_| |_| |____/ \___/  |___|\__|
+       it's just about time
+ 
+        [#]=============================================================[#]
+*/
