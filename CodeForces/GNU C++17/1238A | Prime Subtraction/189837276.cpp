420d64e5cdcd069cbf43ef329f85dd2801ed34f0
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:43:54 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1238A | Prime Subtraction/189837276.cpp
@@ -0,0 +1,53 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+#define fast_IO(x)          ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define fraction            cout<< fixed <<showpoint <<setprecision
+#define what_is(x)          cerr << #x << " is " << x << " ";
+#define PI                  3.141592653589793238462643383279
+#define all(x)              x.begin(),x.end()
+#define Reverse(x)          reverse(all(x))
+#define Sort(x)             sort(all(x))
+#define Yes                 cout<<"YES"
+#define yes                 cout<<"yes"
+#define No                  cout<<"NO"
+#define no                  cout<<"no"
+#define ln                  endl
+typedef long long           ll;
+ 
+//**prototype**//
+inline void _GOM3A_(void);
+//**prototype**//
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll tt;
+    cin >> tt;
+    while(tt--){
+        ll x,y;
+        cin >> x >> y;
+        if( x - y  > 1)
+            Yes<<ln;
+        else
+            No<<ln;
+    }
+    //********** code ***************
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(7);
+    fast_IO(  author: _GOM3A_ );
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+}
