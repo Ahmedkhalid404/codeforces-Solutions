63bf32114c464ad672a7bdfecbafbf1aab1793cb
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:40:55 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/26A | Almost Prime/194475777.cpp
@@ -0,0 +1,199 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_IO(x)              ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fraction                cout<< fixed <<showpoint <<setprecision
+#define what_is(x)              cerr << #x << " is " << x << '\n';
+#define PI                      3.141592653589793238462643383279
+#define Write(x)                freopen (x,"w",stdout)
+#define Read(x)                 freopen (x,"r",stdin)
+#define all(x)                  x.begin(),x.end()
+#define Reverse(x)              reverse(all(x))
+#define Sort(x)                 sort(all(x))
+#define Yes                     cout<<"YES"
+#define yes                     cout<<"yes"
+#define No                      cout<<"NO"
+#define no                      cout<<"no"
+#define pi                      acos(-1);
+#define ln                      '\n'
+typedef deque<long long>        dll;
+typedef vector<long long>       vll;
+typedef set<long long>          sll;
+typedef long long               ll;
+typedef pair<ll,ll>             pl;
+ 
+//**prototype**//
+inline void _GOM3A_(void);
+template<typename T, typename ...Opts>
+bool any_of(T val, Opts ...opts);
+ll getRandomNumber(ll low = 0, ll high = 100);
+bool isPrime(ll n);
+deque<ll> factorization(ll n);
+bool isPalindrome(string s);
+ll binaryToDecimal(string s);
+vector<bool> Sieve(ll n);
+deque< pair<ll,ll> > primeFactorization(ll n);
+inline ll add(ll a , ll b , ll mod);
+inline ll sub(ll a , ll b , ll mod);
+inline ll mul(ll a , ll b , ll mod);
+ll fastPowIterative(ll base ,ll pow);
+//**prototype**//
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll n , result (0);
+    cin >> n;
+    for (int i = 1; i <= n ; ++i) {
+        dll factor = factorization(i);
+        ll cnt = 0;
+        for (int j = 0; j < factor.size(); ++j) {
+            if ( isPrime(factor[j]) )
+                cnt++;
+            if (cnt > 2)
+                break;
+        }
+        if (cnt == 2)
+            result++;
+    }
+    cout<<result;
+    //********** code ***************
+    return 0;
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(6);
+    fast_IO(  author: _GOM3A_ );
+}
+template<typename T,typename ...Opts>
+bool any_of(T val, Opts ...opts)
+{
+    return ( ... || (val == opts) );
+}
+ll getRandomNumber(ll low , ll high ){
+    static ll  fo = 0;
+    if( fo == 92233720368547758 )
+        fo = 0;
+    srand((unsigned) time(NULL) + fo );
+    fo++;
+    return ( ( rand() % (high - low + 1)) + low );
+}
+bool isPrime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    for (ll i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+deque<ll> factorization(ll n){
+    if( n <= 0 )
+    {
+        return (dll {});
+    }
+    if( n == 1 )
+    {
+        dll numbers = {1};
+        return numbers;
+    }
+    set<ll> result;
+    ll i ;
+    for(i = 1; i * i  < n ; i++){
+        if( !( n%i ) )
+        {
+            result.insert(i);
+            result.insert(n/i);
+        }
+    }
+    if(i*i==n)
+        result.insert(i);
+    dll resultInDeque( all(result) );
+    return resultInDeque;
+}
+deque< pair<ll,ll> > primeFactorization(ll n){
+    if( n <= 0 )
+    {
+        return ( deque< pair<ll,ll> > {} );
+    }
+    if( n == 1 )
+    {
+        return ( deque< pair<ll,ll> > {{1, 1}} );
+    }
+    deque< pair<ll,ll> > primeFactor;
+    for (int i = 2; i * i <= n; ++i) {
+        if( !(n % i) )
+            primeFactor.push_back({i,0});
+        while( !( n % i ) )
+        {
+            primeFactor[ primeFactor.size() - 1 ].second ++;
+            n /= i;
+        }
+    }
+    if( n > 1 )
+    {
+        primeFactor.push_back({n,1});
+    }
+    return primeFactor;
+}
+bool isPalindrome(string s)
+{
+    string s2 = s;
+    reverse(s2.begin() , s2.end());
+    return (s2 == s);
+}
+ll binaryToDecimal(string s)
+{
+    bitset<64> bits(s);
+    ll number = bits.to_ulong();
+    return number;
+    //return stol(s,nullptr,2);
+}
+vector<bool> Sieve(ll n)
+{
+    vector<bool> isPrime(n+1 , true);
+    isPrime[0] = isPrime[1] = false;
+    for(ll i = 2 ; i * i <= n ; ++i){
+        if( isPrime[i] )
+            for(int j = i * 2 ; j <= n ; j += i)
+                isPrime[i] = false;
+    }
+    return isPrime;
+}
+inline ll add(ll a , ll b , ll mod)
+{
+    return ( (a % mod) + (b % mod) ) % mod;
+}
+inline ll sub(ll a , ll b , ll mod)
+{
+    return ( (a % mod) - (b % mod) + mod ) % mod;
+}
+inline ll mul(ll a , ll b , ll mod)
+{
+    return ( (a % mod) * (b % mod) ) % mod;
+}
+ll fastPowIterative(ll base,ll pow)//O(log2(n))
+{
+    ll result = 1;
+    while(pow > 0)
+    {
+        if( pow & 1 )
+            result *= base;
+        base = base*base;
+        pow /= 2;
+    }
+    return result;
+}
