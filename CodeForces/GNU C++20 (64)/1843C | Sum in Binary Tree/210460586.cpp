06ec3b8ad437295f52a0eb05180882b955cc5cd5
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:19:49 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1843C | Sum in Binary Tree/210460586.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    ll tt;
+    cin >> tt;
+    while( tt-- )
+    {
+        ll n;
+        cin >> n;
+        ll ans = 0;
+        while( n )
+        {
+            ans += n;
+            n /= 2;
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
