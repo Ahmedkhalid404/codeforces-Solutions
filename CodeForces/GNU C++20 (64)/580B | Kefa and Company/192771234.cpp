47dbb8ad5c78b172c492e4e0a237c65287f9aa22
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:42:10 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/580B | Kefa and Company/192771234.cpp
@@ -0,0 +1,101 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_IO(x)              ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define fraction                cout<< fixed <<showpoint <<setprecision
+#define what_is(x)              cerr << #x << " is " << x << '\n';
+#define PI                      3.141592653589793238462643383279
+#define all(x)                  x.begin(),x.end()
+#define Reverse(x)              reverse(all(x))
+#define Sort(x)                 sort(all(x))
+#define Yes                     cout<<"YES"
+#define yes                     cout<<"yes"
+#define No                      cout<<"NO"
+#define no                      cout<<"no"
+#define ln                      '\n'
+typedef deque<long long>        dll;
+typedef vector<long long>       vll;
+typedef set<long long>          sll;
+typedef long long               ll;
+typedef pair<ll,ll>             pl;
+ 
+//**prototype**//
+inline void _GOM3A_(void);
+template<typename T, typename ...Opts>
+bool any_of(T val, Opts ...opts);
+ll getRandomNumber(ll low = 0, ll high = 100);
+bool isPrime(ll n);
+//**prototype**//
+ 
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll size,k;
+    cin >> size >> k;
+    vector< pair<ll,ll> >numbers(size);
+    for (int i = 0; i < size; ++i) {
+        cin >> numbers[i].first >> numbers[i].second;
+    }
+    Sort(numbers);
+    ll i = 0 , j = 0 , ans = 0;
+    ll mx = 0;
+    while( i < size && j < size){
+        if(numbers[j].first - numbers[i].first < k ){
+            ans += numbers[j].second;
+            mx = max(mx,ans);
+            j++;
+        }
+        else
+        {
+            ans -= numbers[i].second;
+            i++;
+        }
+    }
+    cout<<mx;
+    //********** code ***************
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(6);
+    fast_IO(  author: _GOM3A_ );
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+}
+template<typename T,typename ...Opts>
+bool any_of(T val, Opts ...opts)
+{
+    return ( ... || (val == opts) );
+}
+ll getRandomNumber(ll low , ll high ){
+    static ll  fo = 0;
+    if( fo == 92233720368547758 )
+        fo = 0;
+    srand((unsigned) time(NULL) + fo );
+    fo++;
+    return ( ( rand() % (high - low + 1)) + low );
+}
+bool isPrime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    for (ll i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
