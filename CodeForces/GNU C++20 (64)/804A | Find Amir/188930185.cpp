5ffb8d71badfd7f9adf567e2eb22f365b769d5f2
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:44:37 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/804A | Find Amir/188930185.cpp
@@ -0,0 +1,45 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_IO(x)          ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define fraction            cout<< fixed <<showpoint <<setprecision
+#define SortG(x)            sort(all(x), greater<long long>());
+#define what_is(x)          cerr << #x << " is " << x << "\n"
+#define PI                  3.141592653589793238462643383279
+#define all(x)              x.begin(),x.end()
+#define Reverse(x)          reverse(all(x))
+#define Sort(x)             sort(all(x))
+#define ln                  "\n"
+typedef vector<long long>   vll;
+typedef long long           ll;
+ 
+//**prototype**//
+ 
+inline void _GOM3A_(void);
+ 
+//**prototype**//
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll num;
+    cin >> num;
+    cout<<(num & 1 ? num/2 : num/2 -1 );
+ 
+    //********** code ***************
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(7);
+    fast_IO(  author: _GOM3A_ );
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+}
