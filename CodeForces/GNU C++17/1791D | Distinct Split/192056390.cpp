c4f1372702718cbbf455542600aa920841c16c97
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:42:31 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1791D | Distinct Split/192056390.cpp
@@ -0,0 +1,89 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_IO(x)              ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define fraction                cout<< fixed <<showpoint <<setprecision
+#define what_is(x)              cerr << #x << " is " << x << '\n';
+#define PI                      3.141592653589793238462643383279
+#define all(x)                  x.begin(),x.end()
+#define Reverse(x)              reverse(all(x))
+#define Sort(x)                 sort(all(x))
+#define Yes                     cout<<"YES"
+#define yes                     cout<<"yes"
+#define No                      cout<<"NO"
+#define no                      cout<<"no"
+#define ln                      '\n'
+typedef deque<long long>        dll;
+typedef vector<long long>       vll;
+typedef set<long long>          sll;
+typedef long long               ll;
+typedef pair<ll,ll>             pl;
+ 
+//**prototype**//
+inline void _GOM3A_(void);
+template<typename T, typename ...Opts>
+bool any_of(T val, Opts ...opts);
+template<typename T>
+void doSomeThing(T &num );
+auto getRandomNumber(ll low = 0, ll high = 100);
+//**prototype**//
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll tt;
+    cin >> tt;
+    while(tt--){
+        ll size;
+        string s;
+        cin >> size >> s;
+        map<char,ll>dp;
+        set<char>one;
+        for (int i = 0; i < size; ++i) {
+            dp[ s[i] ]++;
+        }
+        ll mx = 0;
+        for (int i = 0; i < size; ++i) {
+            one.insert( s[i] );
+            dp[ s[i] ]--;
+            if( dp[ s[i] ] <= 0 )
+                dp.erase(s[i]);
+            mx = max( mx , (ll)(dp.size() + one.size()) );
+        }
+        cout<<mx<<ln;
+    }
+    //********** code ***************
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(6);
+    fast_IO(  author: _GOM3A_ );
+    //srand((unsigned) time(NULL));
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+}
+template<typename T,typename ...Opts>
+bool any_of(T val, Opts ...opts)
+{
+    return ( ... || (val == opts) );
+}
+template<typename T>
+void doSomeThing(T & num ){
+    cin >> num;
+}
+auto getRandomNumber(ll low , ll high ){
+    static ll  fo = 0;
+    if( fo == 92233720368547758 )
+        fo = 0;
+    srand((unsigned) time(NULL) + fo );
+    fo++;
+    return ( ( rand() % (high - low + 1)) + low );
+}
