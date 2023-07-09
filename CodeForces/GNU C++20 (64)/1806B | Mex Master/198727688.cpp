408cc5c022ed0d56e5851a3b0724f4f6c6fe0fdf
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:32:45 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1806B | Mex Master/198727688.cpp
@@ -0,0 +1,226 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_IO(x)              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fraction                cout<< fixed <<showpoint <<setprecision
+#define what_is(x)              cerr << #x << " is " << x << '\n';
+#define PI                      3.141592653589793238462643383279
+#define Write(x)                freopen (x,"w",stdout)
+#define __max(x)                (*max_element(all(x)))
+#define __min(x)                (*min_element(all(x)))
+#define Read(x)                 freopen (x,"r",stdin)
+#define all(x)                  x.begin(),x.end()
+#define Reverse(x)              reverse(all(x))
+#define Sort(x)                 sort(all(x))
+#define Yes                     cout<<"YES"
+#define yes                     cout<<"yes"
+#define No                      cout<<"NO"
+#define no                      cout<<"no"
+#define pi                      acos(-1);
+#define ln                      '\n'
+typedef deque<long long>        dll;
+typedef vector<long long>       vll;
+typedef set<long long>          sll;
+typedef long long               ll;
+typedef pair<ll,ll>             pl;
+const   int mod = 1000000007;
+const   ll  OO  = 0x3f3f3f3f3f3f3f3f;
+ 
+//**prototype**//
+inline void _GOM3A_(void);
+deque< pair<ll,ll> > primeFactorization(ll n);
+template<typename T, typename ...Opts>
+bool any_of(T val, Opts ...opts);
+inline ll add(ll a , ll b , ll mod);
+inline ll sub(ll a , ll b , ll mod);
+inline ll mul(ll a , ll b , ll mod);
+deque<ll> factorization(ll n);
+ll binaryToDecimal(string s);
+bool isPalindrome(string s);
+ll fPow(ll base ,ll pow);
+bool isPrime(ll n);
+ll setBit1(ll num,ll idx);
+ll setBit0(ll num ,ll idx);
+ll flipBit(ll num,ll idx);
+bool getBit(ll num,ll idx);
+//**prototype**//
+/*
+  1) __builtin_popcount(ll num )  => counting ones in binary representation
+ */
+void solve(){
+    ll size;
+    cin >> size;
+    vll numbers(size);
+    map<ll,ll>freq;
+    for (int i = 0; i < size; ++i) {
+        cin >> numbers[i];
+        freq[ numbers[i] ]++;
+    }
+    if( freq[0] == 0 )
+    {
+        return void(cout<<0);
+    }
+    if( size == freq[0] )
+    {
+        return void (cout<<1);
+    }
+    if( size - freq[0] >= freq[0] - 1)
+    {
+        return void(cout<<0);
+    }
+    if( size - freq[0] - freq[1] >= 1 )
+        return void (cout<<1);
+    else
+        return void(cout<<2);
+}
+ 
+ 
+int main() {
+    _GOM3A_();
+    //********** code ***************
+    ll tt = 1;
+    cin >> tt;
+    while(tt--)
+    {
+        solve();
+        cout<<ln;
+    }
+    //********** code ***************
+    return 0;
+}
+ 
+//****************************************    helping function    ****************************************
+inline void _GOM3A_(void){
+    fraction(6);
+    fast_IO(  author: _GOM3A_ );
+}
+template<typename T,typename ...Opts>
+bool any_of(T val, Opts ...opts)
+{
+    return ( ... || (val == opts) );
+}
+bool isPrime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    for (ll i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+deque<ll> factorization(ll n){
+    if( n <= 0 )
+    {
+        return (dll {});
+    }
+    if( n == 1 )
+    {
+        return (dll {1});
+    }
+    set<ll> result;
+    ll i ;
+    for(i = 1; i * i < n ; i++){
+        if( !( n%i ) )
+        {
+            result.insert(i);
+            result.insert(n/i);
+        }
+    }
+    if( i * i == n )
+        result.insert(i);
+    return (dll{ all(result) });
+}
+deque< pair<ll,ll> > primeFactorization(ll n){
+    if( n <= 0 )
+    {
+        return ( deque< pair<ll,ll> > {} );
+    }
+    if( n == 1 )
+    {
+        return ( deque< pair<ll,ll> > {{1, 1}} );
+    }
+    deque< pair<ll,ll> > primeFactor;
+    for (int i = 2; i * i <= n; ++i) {
+        if( !(n % i) )
+            primeFactor.push_back({i,0});
+        while( !( n % i ) )
+        {
+            primeFactor[ primeFactor.size() - 1 ].second ++;
+            n /= i;
+        }
+    }
+    if( n > 1 )
+    {
+        primeFactor.push_back({n,1});
+    }
+    return primeFactor;
+}
+bool isPalindrome(string s)
+{
+    ll size = s.size();
+    for(int i = 0 , j = size - 1 ; i < j ; i++,j--)
+    {
+        if( s[i] != s[j] )
+            return false;
+    }
+    return true;
+}
+vector<bool> Sieve(ll n)
+{
+    vector<bool> isPrime(n+1 , true);
+    isPrime[0] = isPrime[1] = false;
+    for(ll i = 2 ; i * i <= n ; ++i){
+        if( isPrime[i] )
+            for(int j = i * 2 ; j <= n ; j += i)
+                isPrime[i] = false;
+    }
+    return isPrime;
+}
+inline ll add(ll a , ll b , ll mod)
+{
+    return ( (a % mod) + (b % mod) ) % mod;
+}
+inline ll sub(ll a , ll b , ll mod)
+{
+    return ( (a % mod) - (b % mod) + mod ) % mod;
+}
+inline ll mul(ll a , ll b , ll mod)
+{
+    return ( (a % mod) * (b % mod) ) % mod;
+}
+ll fPow(ll base,ll pow)
+{
+    if(! ( pow %mod ) ) return 1;
+    cout<<base<<" "<<pow<<ln;
+    ll result = fPow( ( base % mod ) ,(pow % mod ) / 2 ) % mod;
+ 
+    return ( ( pow % mod ) & 1 ? ( (result % mod) * (result % mod) * (base % mod) ) % mod : ( (result % mod) * (result % mod) ) % mod );
+}
+ll setBit1(ll num,ll idx)
+{
+    return ( num | (1 << idx) );
+}
+ll setBit0(ll num ,ll idx)
+{
+    return ( num & ~( 1 << idx ) );
+}
+ll flipBit(ll num,ll idx)
+{
+    return (num ^ (1 << idx) );
+}
+bool getBit(ll num,ll idx)
+{
+    return ( ( num >> idx ) & 1 );
+}
