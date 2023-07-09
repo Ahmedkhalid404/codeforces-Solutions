82100d46db93a4aa90a08b1b046abac7447895f4
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:22:36 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1194B | Yet Another Crosses Problem/206352428.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define HelloWorld(x)           ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);cout<< fixed <<showpoint <<setprecision(7)
+#define ln                      '\n'
+typedef long long               ll;
+ 
+int main() {
+    HelloWorld('♡');
+    ll tt;
+    cin >> tt;
+    while( tt-- )
+    {
+        ll n , m ;
+        cin >> n >> m;
+        vector< vector< char > >grid( n,vector< char >(m) );
+        map< ll , ll >freq,freqCoul;
+        for (int i = 0; i < n; ++i) {
+            ll cnt = 0;
+            for (int j = 0; j < m; ++j) {
+                cin >> grid[i][j];
+                if( grid[i][j] == '.' )
+                    cnt++;
+            }
+            freq[ i ] = cnt;
+        }
+        for (int i = 0; i < m; ++i) {
+            ll cnt = 0;
+            for (int j = 0; j < n; ++j) {
+                if( grid[j][i] == '.' )
+                    cnt++;
+            }
+            freqCoul[ i ] = cnt;
+        }
+        ll res = 100000000000000;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                ll cnt = freq[i] + freqCoul[ j ];
+                if( grid[i][j] == '.' )
+                    cnt--;
+                if( cnt < res )
+                    res = cnt;
+                if( !res )
+                    goto finish;
+            }
+        }
+        finish:;
+        cout<<res<<ln;
+    }
+    return 0;
+}
