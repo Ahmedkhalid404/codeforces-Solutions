9088637440f2566123ce908db1237e862f61d981
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:45:35 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/688B | Lovely Palindromes/187696904.cpp
@@ -0,0 +1,40 @@
+/*
+ *  Show me your beautiful smile :)
+ *
+ *  author:  Ahmed khaled ( _GOM3A_ )
+ *
+ * */
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define Sort(x) sort(all(x))
+#define Reverse(x) reverse(all(x))
+#define ln endl
+#define MAX 500
+#define fraction cout<< fixed <<showpoint <<setprecision
+#define fast_IO(x) ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << " ";
+//#define ONPC
+using namespace std;
+int main() {
+    fraction(7);
+    fast_IO(  author: _GOM3A_ );
+#ifdef ONPC
+    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);
+#endif
+    string n1;
+    cin >> n1;
+    string n2 = n1;
+    Reverse(n2);
+    cout<<n1<<n2;
+ 
+ 
+}
+ 
+//****************************************    helping function    ****************************************
+bool sortbyseconde(const  pair<int,int> &a,const pair<int,int> &b)
+{
+    return a.second>b.second;
+}
+ 
