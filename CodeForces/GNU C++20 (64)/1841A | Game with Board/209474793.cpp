403403fba6372519c8b7eeb6efcc95d9bae88d4d
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2023-07-10 00:20:47 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1841A | Game with Board/209474793.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    ll tt;
+    cin >> tt;
+    while( tt-- )
+    {
+        ll n;
+        cin >> n;
+        if( n <= 4 ) {cout<<"Bob\n";continue;}
+        else {cout<<"Alice\n";continue;}
+ 
+    }
+    return 0;
+}
